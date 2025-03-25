a39c72e9ef8c8127658eadecadd95d214336439e
EdwinESM <a01738380@tec.mx>
2025-03-25 16:04:14 -0600

    Update music.py

--- music.py
+++ music.py
@@ -24,10 +24,12 @@ if __name__ == "__main__":
     query = 'SELECT Class, _Pulseclarity_Mean FROM music WHERE _Pulseclarity_Mean > 0.5 ORDER BY _Pulseclarity_Mean DESC'
     df_pulse_clarity = spark.sql(query)
     df_pulse_clarity.show(20)
+    
+query = 'SELECT * FROM music WHERE _Pulseclarity_Mean > 0.5'
+df_filtered = spark.sql(query)
 
-   results = df_filtered.toJSON().collect()
-    with open('filtered_data.json', 'w') as file:
-        json.dump(results, file)
+results = df_filtered.toJSON().collect()
+with open('filtered_data.json', 'w') as file:
+    json.dump(results, file)
 
-    print("Datos guardados en `filtered_data.json`")
-    spark.stop()
+print("Datos guardados en `filtered_data.json`")
